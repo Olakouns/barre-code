606d1332688bd3407adf865f46ce05fafcca96a8
Razacki Kounasso <rkounasso@metalafrique.local>
2024-01-15 15:40:01 +0000

    Fixed: infinity popup

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
 let myDiv = document.getElementById("result");
 
-Quagga.init(
-  {
-    inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      target: document.getElementById("content"),
-      constraints: {
-        width: 640,
-        height: 480,
-        facingMode: "environment",
-      },
-    },
-    locator: {
-      patchSize: "medium",
-      halfSample: true,
-    },
-    numOfWorkers: navigator.hardwareConcurrency || 4,
-    decoder: {
-      readers: [
-        "code_128_reader",
-        "ean_reader",
-        "ean_8_reader",
-        "code_39_reader",
-        "code_39_vin_reader",
-        "codabar_reader",
-        "upc_reader",
-        "upc_e_reader",
-        "i2of5_reader",
-        "2of5_reader",
-        "code_93_reader",
-      ],
-    },
-    debug: {
-      showCanvas: false,
-      drawBoundingBox: true,
-      showFrequency: false,
-      drawScanline: false,
-      showPattern: false,
-    },
-    locate: true,
-  },
-  function (err) {
-    if (err) {
-      //   myDiv.innerHTML = "Some error for Quagga" + err;
-      console.error(err);
-      alert("Error: No barcode detected. Try again");
-      myDiv.innerText = "Error: No barcode detected. Try again";
-      return;
-    }
-    Quagga.start();
-    // myDiv.innerHTML = "Quagga is started";
-  }
-);
-
+function startInitiBarCode(params) {
+    Quagga.init(
+        {
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.getElementById("content"),
+            constraints: {
+              width: 640,
+              height: 480,
+              facingMode: "environment",
+            },
+          },
+          locator: {
+            patchSize: "medium",
+            halfSample: true,
+          },
+          numOfWorkers: navigator.hardwareConcurrency || 4,
+          decoder: {
+            readers: [
+              "code_128_reader",
+              "ean_reader",
+              "ean_8_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "codabar_reader",
+              "upc_reader",
+              "upc_e_reader",
+              "i2of5_reader",
+              "2of5_reader",
+              "code_93_reader",
+            ],
+          },
+          debug: {
+            showCanvas: false,
+            drawBoundingBox: true,
+            showFrequency: false,
+            drawScanline: false,
+            showPattern: false,
+          },
+          locate: true,
+        },
+        function (err) {
+          if (err) {
+            //   myDiv.innerHTML = "Some error for Quagga" + err;
+            console.error(err);
+            alert("Error: No barcode detected. Try again");
+            myDiv.innerText = "Error: No barcode detected. Try again";
+            return;
+          }
+          Quagga.start();
+          // myDiv.innerHTML = "Quagga is started";
+        }
+      );
+      
+}
 let barrCodeIsDetected = false;
 
-Quagga.onDetected(function (result) {
-    
-  if (barrCodeIsDetected) {
-    return;
-  }
-
+function handleDetect(result) {
+  Quagga.stop();
   barrCodeIsDetected = true;
-  console.log("Barcode detected and processed: ", result.codeResult);
-
   let userResponse = confirm(
     "Barcode detected: " +
       result.codeResult.code +
       "\nDo you want to continue scanning?"
   );
 
-  if (!userResponse) {
-    alert("Scanning ended.");
-    Quagga.stop();
+  if (userResponse) {
+    //alert("Scanning ended.");
+    startInitiBarCode();
   }
   myDiv.innerHTML = "The detected barcode is: " + result.codeResult.code;
-  barrCodeIsDetected = false;
+  //   barrCodeIsDetected = false;
   return;
-});
+}
+
+startInitiBarCode();
+Quagga.onDetected(handleDetect);
